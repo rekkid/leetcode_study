8eae0a1237b0b08ff6ab680147a97a6e39e0cc96
rekkid <zj_jun@163.com>
2019-05-13 20:27:30 +0800

    增加gtest对2sum的测试编码实现

--- 1_2sum.cpp
+++ 1_2sum.cpp
@@ -15,21 +15,41 @@ Example:
     return [0, 1].
 */
 
+#include <unordered_map>
 #include <vector>
 #include <iostream>
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 using namespace std;
+using ::testing::ElementsAre;
 
 class Solution {
  public:
   vector<int> twoSum(vector<int>& nums, int target) {
-
+    //测试
+    unordered_map<int, int> numMap;
+    for (int i = 0; i < nums.size(); ++i) {
+        int delta = target - nums[i];
+        auto it = numMap.find(delta);
+        if (it != numMap.end()) {
+            // 找到
+            return vector<int>{i, it->second};
+        } else {
+            numMap[nums[i]] = i;
+        }
+    }
   }
 };
 
 TEST(Solution, foobar)
 {
-  EXPECT_EQ(1, 1);
+  Solution solution;
+  vector<int> nums{2, 7, 11, 15};
+  vector<int> res = solution.twoSum(nums, 9);
+  for (auto x : res) {
+    cout << x << ",";
+  }
+  ASSERT_THAT(res, ElementsAre(0, 1));
 }
 
